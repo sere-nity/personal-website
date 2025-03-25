cf845fa50515cbad43c8b542441e21918b92e6a3
Serene <serene6197@gmail.com>
2025-03-25 20:11:40 +0000

    fix: remove blog

--- components/home/FeaturedContent.tsx
+++ components/home/FeaturedContent.tsx
@@ -35,23 +35,6 @@ export default function FeaturedContent({
         <p>{description}</p>
       </motion.div>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {items.map((item, index) => (
-          <motion.div
-            key={item.slug}
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-          >
-            {type === 'blog' ? (
-              <BlogCard post={item as BlogPost} />
-            ) : (
-              <ProjectCard project={item as Project} />
-            )}
-          </motion.div>
-        ))}
-      </div>
       
       <motion.div 
         className="text-center mt-12"
